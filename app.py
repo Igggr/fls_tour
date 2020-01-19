eff65708b08e4177a21e64c167d937680336d0ae
Iggr Smirnov <uchetzapis@yahoo.com>
2020-01-19 14:06:46 +0300

    add filter on direction

--- app.py
+++ app.py
@@ -4,21 +4,31 @@ from data import title, subtitle, description, departures, tours
 
 app = Flask('st')
 
+
 @app.route('/')
 def index():
     return render_template('index.html', tours=tours, title=title, subtitle=subtitle, description=description, departures=departures)
+
 
 @app.route('/from/<direction>/')
 def from_direction(direction):
     return render_template('direction.html', direction=direction, departures=departures, tours=tours)
 
+
 @app.route('/tours/<int:id>/')
 def tour_page(id):
     return render_template('tour.html', tour=tours[id], id=id, departures=departures)
 
+
 @app.route("/order/<int:id>/")
 def order(id):
     return f"<h1>page for buying tour {id} is under development</h1>"
+
+
+@app.template_filter("from_direction")
+def from_direction(tours, direction):
+    return dict(filter(lambda p: p[1]['departure'] == direction, tours.items() ))
+
 
 if __name__ == "__main__":
     app.run()

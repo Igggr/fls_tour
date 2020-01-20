6b66e367cf235ff5a473c3e35270910307773718
Iggr Smirnov <uchetzapis@yahoo.com>
2020-01-20 11:27:29 +0300

    add pre-commit hooks

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ app = Flask('st')
 
 @app.route('/')
 def index():
-    return render_template('index.html', tours=tours, title=title, subtitle=subtitle, description=description, departures=departures)
+    return render_template('index.html', tours=tours,
+                           title=title, subtitle=subtitle,
+                           description=description, departures=departures)
 
 
 @app.route('/from/<direction>/')
 def from_direction(direction):
-    return render_template('direction.html', direction=direction, departures=departures, tours=tours)
+    return render_template('direction.html', direction=direction,
+                           departures=departures, tours=tours)
 
 
 @app.route('/tours/<int:id>/')
 def tour_page(id):
-    return render_template('tour.html', tour=tours[id], id=id, departures=departures)
+    return render_template('tour.html', tour=tours[id],
+                           id=id, departures=departures)
 
 
 @app.route("/order/<int:id>/")
@@ -25,11 +29,12 @@ def order(id):
     return f"<h1>page for buying tour {id} is under development</h1>"
 
 
-@app.template_filter("from_direction")
-def from_direction(tours, direction):
-    return dict(filter(lambda p: p[1]['departure'] == direction, tours.items() ))
+@app.template_filter("direction_filter")
+def direction_filter(tours, direction):
+    tours_from_direction = filter(lambda p: p[1]['departure'] == direction,
+                                  tours.items())
+    return dict(tours_from_direction)
 
 
 if __name__ == "__main__":
     app.run()
-

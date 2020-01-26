cab395cc0bcc0f89e5a4ee0a80395421962bc40d
Iggr Smirnov <uchetzapis@yahoo.com>
2020-01-26 19:45:06 +0300

    get rid of some extra whitespace + trim requirements

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def from_direction(direction):
     return render_template('direction.html', direction=direction,
                            departures=departures, tours=tours)
 
-
 @app.route('/tours/<int:id>/')
 def tour_page(id):
     return render_template('tour.html', tour=tours[id],
